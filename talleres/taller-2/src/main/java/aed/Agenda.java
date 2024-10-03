21db56af32dfadd3692046af3403b8c013a417da
Marcos <mquiroga@dc.uba.ar>
2024-10-03 13:04:02 -0300

    Se completaron los ejercicios de agenda

--- talleres/taller-2/src/main/java/aed/Agenda.java
+++ talleres/taller-2/src/main/java/aed/Agenda.java
@@ -2,27 +2,49 @@ package aed;
 
 public class Agenda {
 
+    private Fecha f;
+    private Recordatorio [] r = new Recordatorio[0];
+
     public Agenda(Fecha fechaActual) {
-        // Implementar
+        f = new Fecha(fechaActual.dia(), fechaActual.mes());
     }
 
     public void agregarRecordatorio(Recordatorio recordatorio) {
-        // Implementar
+        Recordatorio[] nuevoR = new Recordatorio[r.length + 1];
+
+        for(int j = 0; j < r.length; j++){
+            nuevoR[j] = r[j];
+        }
+
+        nuevoR[nuevoR.length - 1] = new Recordatorio(recordatorio.mensaje(), recordatorio.fecha(), recordatorio.horario());
+        r = nuevoR;
     }
 
     @Override
     public String toString() {
-        // Implementar
-        return "";
+        String recos = "";
+
+        for(int j = 0; j < r.length; j++){
+            if(f.dia() == r[j].fecha().dia() && f.mes() == r[j].fecha().mes()){
+                recos = recos + r[j].toString().concat("\n");
+            }
+            
+        }
+
+  
+        return f
+                .toString()
+                .concat("\n=====\n")
+                .concat(recos);
     }
 
     public void incrementarDia() {
-        // Implementar
+        f.incrementarDia();
     }
 
     public Fecha fechaActual() {
-        // Implementar
-        return null;
+    
+        return f;
     }
 
 }

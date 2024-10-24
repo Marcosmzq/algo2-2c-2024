5ee1df25c15fd5aa6838128caf3991d802cc51da
Marcos <mquiroga@dc.uba.ar>
2024-10-24 15:38:37 -0300

    Se resolvio el primer ejercicio y se agregaron los atributos de ABB, Nodo y el constructor de Nodo

--- talleres/taller-4/src/main/java/aed/ABB.java
+++ talleres/taller-4/src/main/java/aed/ABB.java
@@ -5,53 +5,100 @@ import java.util.*;
 // Todos los tipos de datos "Comparables" tienen el método compareTo()
 // elem1.compareTo(elem2) devuelve un entero. Si es mayor a 0, entonces elem1 > elem2
 public class ABB<T extends Comparable<T>> implements Conjunto<T> {
-    // Agregar atributos privados del Conjunto
+
+    private Nodo _raiz;
+    private int _cardinal;
+    private int _altura;
 
     private class Nodo {
-        // Agregar atributos privados del Nodo
+        private T valor;
+        private Nodo izq;
+        private Nodo der;
+        private Nodo padre;
 
-        // Crear Constructor del nodo
+        public Nodo(T v) {
+            valor = v;
+            izq = null;
+            der = null;
+            padre = null;
+        }
     }
 
     public ABB() {
-        throw new UnsupportedOperationException("No implementada aun");
+        _raiz = null;
+        _cardinal = 0;
+        _altura = 0;
     }
 
     public int cardinal() {
+        return _cardinal;
+    }
+
+    public T minimo() {
+
+        if (_raiz == null) {
+            return _raiz.valor;
+        } else {
+            Nodo actual = _raiz;
+            while (actual.izq != null) {
+                actual = actual.izq;
+            }
+            return actual.valor;
+        }
+    }
+
+    public T maximo() {
+
+        if (_raiz == null) {
+            return _raiz.valor;
+        } else {
+            Nodo actual = _raiz;
+            while (actual.der != null) {
+                actual = actual.der;
+            }
+            return actual.valor;
+        }
+    }
+
+    public void insertar(T elem) {
         throw new UnsupportedOperationException("No implementada aun");
     }
 
-    public T minimo(){
+    public boolean pertenece(T elem) {
+
+        Nodo actual = _raiz;
+
+        while (actual != null) {
+
+            int resCompare = actual.valor.compareTo(elem);
+
+            if (resCompare > 0) {
+                actual = actual.izq;
+            } else if (resCompare < 0) {
+                actual = actual.der;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void eliminar(T elem) {
         throw new UnsupportedOperationException("No implementada aun");
     }
 
-    public T maximo(){
-        throw new UnsupportedOperationException("No implementada aun");
-    }
-
-    public void insertar(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
-    }
-
-    public boolean pertenece(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
-    }
-
-    public void eliminar(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
-    }
-
-    public String toString(){
+    public String toString() {
         throw new UnsupportedOperationException("No implementada aun");
     }
 
     private class ABB_Iterador implements Iterador<T> {
         private Nodo _actual;
 
-        public boolean haySiguiente() {            
+        public boolean haySiguiente() {
             throw new UnsupportedOperationException("No implementada aun");
         }
-    
+
         public T siguiente() {
             throw new UnsupportedOperationException("No implementada aun");
         }
